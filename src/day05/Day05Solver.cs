9793766137eee197b6684bb2546b362e0581740a
Corentin Humbert <corentin.humbert7@gmail.com>
2023-12-06 11:45:21 +0100

    style: fix formatting for solver 5

--- src/day05/Day05Solver.cs
+++ src/day05/Day05Solver.cs
@@ -2,14 +2,14 @@ using System.Collections;
 
 namespace aoc_2023.src.day05
 {
-	public class Day05Solver : AocSolver
-	{
-		// Link to the puzzle: https://adventofcode.com/2023/day/5
+    public class Day05Solver : AocSolver
+    {
+        // Link to the puzzle: https://adventofcode.com/2023/day/5
 
-		public override int Day => 5;
-		public override void Solve()
-		{
-			string[] lines = ReadInputLines("input", true);
+        public override int Day => 5;
+        public override void Solve()
+        {
+            string[] lines = ReadInputLines("input", true);
 
             var parts = lines[0].Split(':');
             if (parts[0] != "seeds") throw new Exception("WTF");
@@ -22,203 +22,203 @@ namespace aoc_2023.src.day05
             Console.WriteLine();
 
             var transformations = new List<long[]>() { seeds };
-			long[] currentSources = Array.Empty<long>();
-			bool[] hasBeenMapped = new bool[seeds.Length];
+            long[] currentSources = Array.Empty<long>();
+            bool[] hasBeenMapped = new bool[seeds.Length];
 
             for (int i = 2; i < lines.Length; i++)
-			{
-				string line = lines[i];
-				if (line.Contains(':')) // Map section start
-				{
-					var previousSources = transformations.Last();
+            {
+                string line = lines[i];
+                if (line.Contains(':')) // Map section start
+                {
+                    var previousSources = transformations.Last();
                     currentSources = new long[previousSources.Length];
-					for (int j = 0; j < currentSources.Length; j++)
-					{
+                    for (int j = 0; j < currentSources.Length; j++)
+                    {
                         currentSources[j] = previousSources[j];
-						hasBeenMapped[j] = false;
+                        hasBeenMapped[j] = false;
                     }
                 }
                 else if (string.IsNullOrEmpty(line)) // end map
-				{
-					transformations.Add(currentSources);
-					for (int j = 0; j < currentSources.Length; j++)
-					{
-						Console.Write(currentSources[j] + " ");
+                {
+                    transformations.Add(currentSources);
+                    for (int j = 0; j < currentSources.Length; j++)
+                    {
+                        Console.Write(currentSources[j] + " ");
                     }
-					Console.WriteLine();
-				}
-				else
-				{
-					var mapSections = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+                    Console.WriteLine();
+                }
+                else
+                {
+                    var mapSections = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
 
-					long length = mapSections[2];
+                    long length = mapSections[2];
 
-					long destinationRangeStart = mapSections[0];
-					long destinationRangeEnd = destinationRangeStart + length;
+                    long destinationRangeStart = mapSections[0];
+                    long destinationRangeEnd = destinationRangeStart + length;
 
-					long sourceRangeStart = mapSections[1];
-					long sourceRangeEnd = sourceRangeStart + length;
+                    long sourceRangeStart = mapSections[1];
+                    long sourceRangeEnd = sourceRangeStart + length;
 
-					long mapTransfo = destinationRangeStart - sourceRangeStart;
+                    long mapTransfo = destinationRangeStart - sourceRangeStart;
 
-					for (int j = 0; j < currentSources.Length; j++)
-					{
-						var item = currentSources[j];
-						if (item >= sourceRangeStart && item <= sourceRangeEnd && !hasBeenMapped[j])
-						{
-							long mappedItem = item + mapTransfo;
+                    for (int j = 0; j < currentSources.Length; j++)
+                    {
+                        var item = currentSources[j];
+                        if (item >= sourceRangeStart && item <= sourceRangeEnd && !hasBeenMapped[j])
+                        {
+                            long mappedItem = item + mapTransfo;
                             currentSources[j] = mappedItem;
-							hasBeenMapped[j] = true;
+                            hasBeenMapped[j] = true;
                         }
                     }
-				}
-			}
+                }
+            }
 
-			var finalResults = transformations.Last();
-			var minNum = finalResults.Min();
+            var finalResults = transformations.Last();
+            var minNum = finalResults.Min();
 
-			Console.WriteLine($"Result is {minNum}");
-		}
+            Console.WriteLine($"Result is {minNum}");
+        }
 
-		public override void SolvePart2()
-		{
-			string[] lines = ReadInputLines("input", true);
+        public override void SolvePart2()
+        {
+            string[] lines = ReadInputLines("input", true);
 
             var parts = lines[0].Split(':');
             if (parts[0] != "seeds") throw new Exception("WTF");
 
             long[] seedDescriptors = parts[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
 
-			long totalSizeArr = 0;
-			for (int i = 1; i < seedDescriptors.Length; i += 2)
-			{
-				totalSizeArr += seedDescriptors[i];
-			}
-			Console.WriteLine($"Total seeds: {totalSizeArr}");
+            long totalSizeArr = 0;
+            for (int i = 1; i < seedDescriptors.Length; i += 2)
+            {
+                totalSizeArr += seedDescriptors[i];
+            }
+            Console.WriteLine($"Total seeds: {totalSizeArr}");
 
-			long[] seeds = new long[totalSizeArr];
-			int arrayIt = 0;
-			Console.WriteLine($"Generating the {totalSizeArr} seeds...");
+            long[] seeds = new long[totalSizeArr];
+            int arrayIt = 0;
+            Console.WriteLine($"Generating the {totalSizeArr} seeds...");
 
-			for (int i = 0; i < seedDescriptors.Length; i += 2)
-			{
-				long rangeStart = seedDescriptors[i];
-				long rangeLen = seedDescriptors[i + 1];
+            for (int i = 0; i < seedDescriptors.Length; i += 2)
+            {
+                long rangeStart = seedDescriptors[i];
+                long rangeLen = seedDescriptors[i + 1];
 
-				totalSizeArr += rangeLen;
+                totalSizeArr += rangeLen;
 
-				for (long j = rangeStart; j < rangeStart + rangeLen; j++)
-				{
-					seeds[arrayIt++] = j;	
-				}
-			}
+                for (long j = rangeStart; j < rangeStart + rangeLen; j++)
+                {
+                    seeds[arrayIt++] = j;
+                }
+            }
 
-			Console.WriteLine("Full seed array generated");
+            Console.WriteLine("Full seed array generated");
 
-			long[] currentSources = seeds;
+            long[] currentSources = seeds;
 
-			var currentMap = new SeedMap();
+            var currentMap = new SeedMap();
             for (int i = 2; i < lines.Length; i++)
-			{
-				string line = lines[i];
-				if (line.Contains(':')) // Map section start
-				{
-					currentMap = new SeedMap();
+            {
+                string line = lines[i];
+                if (line.Contains(':')) // Map section start
+                {
+                    currentMap = new SeedMap();
                 }
                 else if (string.IsNullOrEmpty(line)) // end map
-				{
-					Console.WriteLine("First map constructed");
-					for (int j = 0; j < currentSources.Length; j++)
-					{
-						long item = currentSources[j];
-						long transformed = currentMap.Map(item);
-						currentSources[j] = transformed;
-						//Console.Write(transformed + " ");
-					}
-					Console.WriteLine("Finished first mapping, line is " + i);
-				}
-				else
-				{
-					currentMap.AddRawMapping(line);
-				}
-			}
+                {
+                    Console.WriteLine("First map constructed");
+                    for (int j = 0; j < currentSources.Length; j++)
+                    {
+                        long item = currentSources[j];
+                        long transformed = currentMap.Map(item);
+                        currentSources[j] = transformed;
+                        //Console.Write(transformed + " ");
+                    }
+                    Console.WriteLine("Finished first mapping, line is " + i);
+                }
+                else
+                {
+                    currentMap.AddRawMapping(line);
+                }
+            }
 
-			var finalResults = currentSources;// transformations.Last();
-			var minNum = finalResults.Min();
+            var finalResults = currentSources;// transformations.Last();
+            var minNum = finalResults.Min();
 
-			Console.WriteLine($"Result for part 2 is {minNum}");
-		}
+            Console.WriteLine($"Result for part 2 is {minNum}");
+        }
 
-		public class SeedMap
-		{
-			public List<SeedMapping> Mappings { get; set; } = new List<SeedMapping>();
+        public class SeedMap
+        {
+            public List<SeedMapping> Mappings { get; set; } = new List<SeedMapping>();
 
-			public void AddRawMapping(string rawLine)
-			{
-				var newMapping = new SeedMapping(rawLine);
-				Mappings.Add(newMapping);
-				Mappings.Sort();
-			}
+            public void AddRawMapping(string rawLine)
+            {
+                var newMapping = new SeedMapping(rawLine);
+                Mappings.Add(newMapping);
+                Mappings.Sort();
+            }
 
-			public long Map(long item)
-			{
-				for (int i = 0; i < Mappings.Count; i++)
-				{
-					var mapping = Mappings[i];
-					if (item < mapping.SourceStart) return item;
-					
-					if (item >= mapping.SourceStart && item < mapping.SourceEnd)
-					{
-						return mapping.TrustedTransform(item);
-					}
-				}
+            public long Map(long item)
+            {
+                for (int i = 0; i < Mappings.Count; i++)
+                {
+                    var mapping = Mappings[i];
+                    if (item < mapping.SourceStart) return item;
 
-				return item;
-			}
-		}
+                    if (item >= mapping.SourceStart && item < mapping.SourceEnd)
+                    {
+                        return mapping.TrustedTransform(item);
+                    }
+                }
 
-		public class SeedMapping : IComparable<SeedMapping>
-		{
-			public long SourceStart { get; set; }
-			public long SourceEnd { get; set; }
-			public long DestinationStart { get; set; }
-			public long DestinationEnd { get; set; }
-			public long Length { get; set; }
+                return item;
+            }
+        }
 
-			public SeedMapping(int sourceStart, int destinationStart, int length)
-			{
-				SourceStart = sourceStart;
-				SourceEnd = sourceStart + length;
-				DestinationStart = destinationStart;
-				DestinationEnd = destinationStart + length;
-				Length = length;
-			}
+        public class SeedMapping : IComparable<SeedMapping>
+        {
+            public long SourceStart { get; set; }
+            public long SourceEnd { get; set; }
+            public long DestinationStart { get; set; }
+            public long DestinationEnd { get; set; }
+            public long Length { get; set; }
 
-			public SeedMapping(string rawLine)
-			{
-				var parts = rawLine.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(part => long.Parse(part)).ToArray();
-				Length = parts[2];
+            public SeedMapping(int sourceStart, int destinationStart, int length)
+            {
+                SourceStart = sourceStart;
+                SourceEnd = sourceStart + length;
+                DestinationStart = destinationStart;
+                DestinationEnd = destinationStart + length;
+                Length = length;
+            }
 
-				SourceStart = parts[1];
-				SourceEnd = parts[1] + parts[2];
-				DestinationStart = parts[0];
-				DestinationEnd = parts[0] + parts[2];
-			}
+            public SeedMapping(string rawLine)
+            {
+                var parts = rawLine.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(part => long.Parse(part)).ToArray();
+                Length = parts[2];
 
-			public long TrustedTransform(long item)
-			{
-				long diff = DestinationStart - SourceStart;
-				return item + diff;
-			}
+                SourceStart = parts[1];
+                SourceEnd = parts[1] + parts[2];
+                DestinationStart = parts[0];
+                DestinationEnd = parts[0] + parts[2];
+            }
+
+            public long TrustedTransform(long item)
+            {
+                long diff = DestinationStart - SourceStart;
+                return item + diff;
+            }
 
             public int CompareTo(SeedMapping? other)
             {
                 if (other == null) return 1;
 
-				if (other.SourceStart > SourceStart) return -1;
-				if (other.SourceStart < SourceStart) return 1;
+                if (other.SourceStart > SourceStart) return -1;
+                if (other.SourceStart < SourceStart) return 1;
 
-				return 0;
+                return 0;
             }
 
             public override string ToString()
@@ -226,5 +226,5 @@ namespace aoc_2023.src.day05
                 return $"{SourceStart} - {SourceEnd} : {DestinationStart} - {DestinationEnd} ({Length})";
             }
         }
-	}
+    }
 }
